fdd2c56459fc7d39182b011f619a5d6b30d29f68
Andy <andy@Andys-MacBook-Pro.local>
2022-07-24 12:34:49 -0500

    Refactor: rename DependencyList to DependencyApp to better refelect utility.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import DependencyList from './components/DependencyList';
+import DependencyList from './components/DependencyApp';
+import DependencyApp from './components/DependencyApp';
 
 function App() {
   return (
     <div className="App">
-      <DependencyList></DependencyList>
+      <DependencyApp></DependencyApp>
     </div>
   );
 }
